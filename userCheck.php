38691ed4541621190b1a94cafc5bbf5633aaba34
Jose Diaz <jodiaz@csumb.edu>
2015-03-21 03:40:30 -0700

    Fixed query for date range, view assignments due within 7 days

--- userCheck.php
+++ userCheck.php
@@ -19,13 +19,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST" )
 			$statement = $dbconn->query($sql);
 		}
 		  passthru("python generateAssignments.py 1 2>&1".$studentOtterID." ".$studentpassword);
-		  $sql = "select classes.className,assign.assignmentName, STR_TO_DATE(assign.dueDate,'%d %M %Y') as date
+		  $sql = "select DISTINCT classes.className,assign.assignmentName, STR_TO_DATE(assign.dueDate,'%d %M %Y') as date
 					from ild_assignment assign 
 					inner join ild_classes classes on classes.classId=assign.classId 		
 					inner join ild_assignment_grade sa on assign.assignmentId=sa.assignmentId		
 					inner join ild_students students on sa.studentId = students.studentId		
-					where students.otterId = '$studentOtterID'
-					limit 10";
+                    where students.otterId = '$studentOtterID' 
+					AND 
+                    STR_TO_DATE(assign.dueDate,'%d %M %Y') BETWEEN SYSDATE() AND ADDDATE(SYSDATE(), INTERVAL 7 DAY)
+					ORDER BY className";
 			$x = $dbconn->query($sql);
 			$items = $x -> fetchAll(PDO::FETCH_ASSOC);
 
