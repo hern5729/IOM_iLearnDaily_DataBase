1d106cf256076d3917738176bd7aed051cc3137b
Daniel Pesqueda <dpesqueda@csumb.edu>
2015-03-22 21:43:00 -0700

    Added Correct format for mysql query
    Fixed some things

--- generateAssignments.py
+++ generateAssignments.py
@@ -85,8 +85,9 @@ try:
             tempClass.set_courseName(arr[index])
             tempArr = each.split(",")
             if len(tempArr) > 1:
-                query = ("select exists(select assignmentName from ild_assignment where assignmentName = '"+tempArr[0]+"') as exiists")
-                cursor.execute(query)
+                query = ("select exists(select assignmentName from ild_assignment where assignmentName = '%s') as exiists")
+                querya = (tempArr[0])
+                cursor.execute(query, querya)
                 result = cursor.fetchall()[0][0]
                 if result == 0:
                     tempClass.set_assignmentName(tempArr[0])
